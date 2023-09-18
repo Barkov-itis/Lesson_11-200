a7d841462d5d52863c66fec81ccb46299fac5e5f
danil-11906 <tron5268@mail.ru>
2023-09-18 09:55:33 +0300

    maven
    maven

---
+++ src/main/java/ru/itis/TestServlet.java
@@ -0,0 +1,24 @@
+package ru.itis;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/profile")
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/html/profile.html").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("first_name");
+        String secondName = request.getParameter("second_name");
+        System.out.println(firstName + " " + secondName);
+    }
+}
